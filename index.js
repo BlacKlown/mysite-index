3cf887c8dc373f029c66f4170e02ba8b9112011f
heben <867337666@qq.com>
2018-08-22 17:03:32 +0800

    loadingPic debug
    1.把window.onload触发事件改成了document.onreadystatechange触发事件。
    2.修复了加载动画在页面加载完成之后才显示的BUG。

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
-window.onload = function(){
+document.onreadystatechange = function(){
 	var main = document.getElementById('main'),
 		cover = document.getElementById('cover'),
 		loading = document.getElementById('loading');
 	
-	loading.style.display = "block";
-	loading.style.opacity = 1;
-	myFadeOut();
+	if (document.readyState == "interactive") {
+		loading.style.display = "block";
+		loading.style.opacity = 1;
+		
+	}else if (document.readyState == "complete") {
+		myFadeOut();
 
-	cover.onmouseover = function(){
-		cover.onmousemove = function(event){
-			event = event || window.event;
-			var mX = 240 - event.clientX + main.offsetLeft,
-				mY = 135 - event.clientY + main.offsetTop;
-				rY = "rotateY(" + mX/10 + "deg) ",
-				tX = "translateX(" + (-mX/10) + "px) ",
-				rX = "rotateX(" + (-mY/10) + "deg)",
-				tY = "translateY(" + (-mY/10) + "px)";
-			main.style.transform = "";
-			main.style.transform += rY;
-			main.style.transform += tX;
-			main.style.transform += rX;
-			main.style.transform += tY;
+		cover.onmouseover = function(){
+			cover.onmousemove = function(event){
+				event = event || window.event;
+				var mX = 240 - event.clientX + main.offsetLeft,
+					mY = 135 - event.clientY + main.offsetTop;
+					rY = "rotateY(" + mX/10 + "deg) ",
+					tX = "translateX(" + (-mX/10) + "px) ",
+					rX = "rotateX(" + (-mY/10) + "deg)",
+					tY = "translateY(" + (-mY/10) + "px)";
+				main.style.transform = "";
+				main.style.transform += rY;
+				main.style.transform += tX;
+				main.style.transform += rX;
+				main.style.transform += tY;
+			}
 		}
-	}
-	cover.onmouseout = function(){
-		main.onmousemove = null;
-		main.style.transform = "none";
+		cover.onmouseout = function(){
+			main.onmousemove = null;
+			main.style.transform = "none";
+		}
 	}
 }
 function myFadeOut(){
