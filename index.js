5db47ea2f5dadc114ca624918a9825271baead2d
heben <867337666@qq.com>
2018-11-06 11:14:33 +0800

    重新设计+响应式

--- index.js
+++ index.js
@@ -9,27 +9,6 @@ document.onreadystatechange = function(){
 		
 	}else if (document.readyState == "complete") {
 		myFadeOut();
-
-		cover.onmouseover = function(){
-			cover.onmousemove = function(event){
-				event = event || window.event;
-				var mX = 240 - event.clientX + main.offsetLeft,
-					mY = 135 - event.clientY + main.offsetTop;
-					rY = "rotateY(" + mX/10 + "deg) ",
-					tX = "translateX(" + (-mX/10) + "px) ",
-					rX = "rotateX(" + (-mY/10) + "deg)",
-					tY = "translateY(" + (-mY/10) + "px)";
-				main.style.transform = "";
-				main.style.transform += rY;
-				main.style.transform += tX;
-				main.style.transform += rX;
-				main.style.transform += tY;
-			}
-		}
-		cover.onmouseout = function(){
-			main.onmousemove = null;
-			main.style.transform = "none";
-		}
 	}
 }
 function myFadeOut(){
